d732c05a0ec650225ed66b9e5fcc6d3fba38ee01
Alexandre Batistella Bellas <alexandre.bellas@usp.br>
2021-10-18 01:04:44 +0200

    feat(bling/main): add main bling class into the project as TS file

---
+++ src/bling.ts
@@ -0,0 +1,45 @@
+'use strict'
+
+import Products from './entities/products'
+import Orders from './entities/orders'
+import axios, { AxiosInstance } from 'axios'
+
+import createError from './core/createError'
+
+export default class Bling {
+  private api: AxiosInstance
+
+  constructor(apiKey: string) {
+    if (!apiKey || typeof apiKey !== 'string') {
+      throw createError(
+        "The API key wasn't correctly provided for Bling connection.",
+        500,
+        this,
+        'ERR_NO_API_KEY'
+      )
+    }
+
+    const api = axios.create({
+      baseURL: 'https://bling.com.br/Api/v2'
+    })
+
+    api.interceptors.request.use((config) => {
+      config.params = {
+        apikey: apiKey,
+        ...config.params
+      }
+
+      return config
+    })
+
+    this.api = api
+  }
+
+  public products() {
+    return new Products(this.api)
+  }
+
+  public orders() {
+    return new Orders(this.api)
+  }
+}
