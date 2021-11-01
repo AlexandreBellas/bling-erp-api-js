28d38ef864836270829cb0c557ea5643cbc8f6ae
Alexandre Batistella Bellas <alexandre.bellas@usp.br>
2021-11-01 23:33:14 +0100

    test: turn into synchronous some tests related to the find function of contacts

--- test/entities/contacts/find.test.ts
+++ test/entities/contacts/find.test.ts
@@ -37,58 +37,46 @@ test.concurrent(
   }
 )
 
-test.concurrent(
-  "shouldn't find a contact when calling `.find()` method with an inexistent id with beautified response",
-  async () => {
-    try {
-      await bling
-        .contacts()
-        .find('0', { params: { identificador: '1' }, raw: false })
-      expect(false).toBe(true)
-    } catch (err) {
-      testError(err as IBlingError)
-    }
+test("shouldn't find a contact when calling `.find()` method with an inexistent id with beautified response", async () => {
+  try {
+    await bling
+      .contacts()
+      .find('0', { params: { identificador: '1' }, raw: false })
+    expect(false).toBe(true)
+  } catch (err) {
+    testError(err as IBlingError)
   }
-)
+})
 
-test.concurrent(
-  "shouldn't find a contact when calling `.find()` method with an inexistent CPF/CNPJ with beautified response",
-  async () => {
-    try {
-      await bling
-        .contacts()
-        .find('0', { params: { identificador: '2' }, raw: false })
-      expect(false).toBe(true)
-    } catch (err) {
-      testError(err as IBlingError)
-    }
+test("shouldn't find a contact when calling `.find()` method with an inexistent CPF/CNPJ with beautified response", async () => {
+  try {
+    await bling
+      .contacts()
+      .find('0', { params: { identificador: '2' }, raw: false })
+    expect(false).toBe(true)
+  } catch (err) {
+    testError(err as IBlingError)
   }
-)
+})
 
-test.concurrent(
-  "shouldn't find a contact when calling `.find()` method with an inexistent id with raw response",
-  async () => {
-    try {
-      await bling
-        .contacts()
-        .find('0', { params: { identificador: '1' }, raw: true })
-      expect(false).toBe(true)
-    } catch (err) {
-      testError(err as IBlingError)
-    }
+test("shouldn't find a contact when calling `.find()` method with an inexistent id with raw response", async () => {
+  try {
+    await bling
+      .contacts()
+      .find('0', { params: { identificador: '1' }, raw: true })
+    expect(false).toBe(true)
+  } catch (err) {
+    testError(err as IBlingError)
   }
-)
+})
 
-test.concurrent(
-  "shouldn't find a contact when calling `.find()` method with an inexistent CPF/CNPJ with raw response",
-  async () => {
-    try {
-      await bling
-        .contacts()
-        .find('0', { params: { identificador: '2' }, raw: true })
-      expect(false).toBe(true)
-    } catch (err) {
-      testError(err as IBlingError)
-    }
+test("shouldn't find a contact when calling `.find()` method with an inexistent CPF/CNPJ with raw response", async () => {
+  try {
+    await bling
+      .contacts()
+      .find('0', { params: { identificador: '2' }, raw: true })
+    expect(false).toBe(true)
+  } catch (err) {
+    testError(err as IBlingError)
   }
-)
+})
