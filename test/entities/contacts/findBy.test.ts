e3ecf04a4f1be2e1a14382d0ff704ffaed87012e
Alexandre Batistella Bellas <alexandre.bellas@usp.br>
2022-03-13 04:58:33 +0100

    fix(tests): fix module usage in previously written tests to follow correctly the new features

--- test/entities/contacts/findBy.test.ts
+++ test/entities/contacts/findBy.test.ts
@@ -4,8 +4,8 @@ jest.setTimeout(60000)
 
 test('should find contacts when calling `.findBy()` method with options', async () => {
   await expect(
-    bling
-      .contacts()
-      .findBy({ tipoPessoa: 'F', dataInclusao: '01/08/2021 TO 31/08/2021' })
+    bling.contacts().findBy({
+      filters: { tipoPessoa: 'F', dataInclusao: '01/08/2021 TO 31/08/2021' }
+    })
   ).resolves.toBeDefined()
 })
--- test/package/main-module.test.ts
+++ test/package/main-module.test.ts
@@ -1,14 +1,25 @@
-import { Bling } from '../../lib/bling'
+import { Bling, IBlingError } from '../config/bling'
 
 jest.setTimeout(50000)
+
+const testError = (err: IBlingError) => {
+  expect(err.message).toEqual(
+    'Error on all method during request call: Request failed with status code 401'
+  )
+  expect(err.code).toEqual('ERR_GET_REQUEST_FAILURE')
+  expect(err.status).toEqual(401)
+}
 
 test.concurrent(
   'should fail when an ordinary request is made with a bad API key',
   async () => {
     const bling = new Bling('1234')
 
-    await expect(bling.products().all()).rejects.toThrow(
-      'Request failed with status code 401'
-    )
+    try {
+      await bling.products().all()
+      expect(false).toBe(true)
+    } catch (err) {
+      testError(err as IBlingError)
+    }
   }
 )
