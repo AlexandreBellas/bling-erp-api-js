c582cf501ef3339cfadc093ca027f418123f7795
Alexandre Batistella Bellas <alexandre.bellas@usp.br>
2022-05-29 13:51:33 -0300

    refactor(generators): update contacts generator with template attributes

--- test/generators/contacts.js
+++ test/generators/contacts.js
@@ -1,26 +1,26 @@
+// Global imports
 import chance from 'chance'
 import nullable from './helpers/nullable'
 
-import { CPF, CNPJ } from 'cpf_cnpj'
+// Template attributes
+import uf from './template/uf'
+import tipoPessoa from './template/tipoPessoa'
+import cpfCnpj from './template/cpfCnpj'
+import contribuinte from './template/contribuinte'
 
+// Config
 const seed = chance()
 
+// Generator
 const contact = () => {
-  const tipoPessoa = seed.pickone(['F', 'J'])
-  let cpfCnpj
-
-  if (tipoPessoa === 'F') {
-    cpfCnpj = CPF.generate()
-  } else {
-    cpfCnpj = CNPJ.generate()
-  }
+  const tipoPessoaValue = tipoPessoa()
 
   return {
     nome: seed.sentence({ words: 3 }),
     fantasia: nullable(seed.sentence({ words: 3 }), 30),
-    tipoPessoa,
-    contribuinte: seed.pickone(['1', '2', '9']),
-    cpf_cnpj: cpfCnpj,
+    tipoPessoa: tipoPessoaValue,
+    contribuinte: contribuinte(),
+    cpf_cnpj: cpfCnpj(tipoPessoaValue),
     ie_rg: nullable(seed.integer({ min: 100000000000, max: 199999999999 }), 30),
     endereco: seed.street(),
     numero: nullable(seed.integer({ min: 1, max: 500 }), 30),
@@ -28,37 +28,7 @@ const contact = () => {
     bairro: seed.province(),
     cep: seed.zip(),
     cidade: nullable(seed.city(), 30),
-    uf: nullable(
-      seed.pickone([
-        'AC',
-        'AL',
-        'AP',
-        'AM',
-        'BA',
-        'CE',
-        'DF',
-        'ES',
-        'GO',
-        'MA',
-        'MT',
-        'MS',
-        'MG',
-        'PA',
-        'PB',
-        'PR',
-        'PE',
-        'PI',
-        'RJ',
-        'RN',
-        'RS',
-        'RO',
-        'RR',
-        'SC',
-        'SP',
-        'SE',
-        'TO'
-      ])
-    ),
+    uf: nullable(uf()),
     fone: seed.string({ length: 8, alpha: false }),
     celular: nullable(seed.string({ length: 9, alpha: false }), 10),
     email: nullable(seed.email()),
