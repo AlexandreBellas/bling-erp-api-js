adb846d8aa6a9ba553d6bf8997aaeb190f4543a3
Alexandre Batistella Bellas <alexandre.bellas@usp.br>
2022-08-20 14:16:13 -0300

    feat(helpers): add generic type on returning value of nullable helper function

--- test/generators/helpers/nullable.ts
+++ test/generators/helpers/nullable.ts
@@ -2,6 +2,6 @@ import chance from 'chance'
 
 const seed = chance()
 
-export default (value: unknown, likelihood = 50) => {
+export default <T>(value: T, likelihood = 50): T | null => {
   return seed.bool({ likelihood }) ? value : null
 }
