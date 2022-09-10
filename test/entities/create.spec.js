5d59854ca072d85db04c19913492abb7002d7a2f
Alexandre Batistella Bellas <alexandre.bellas@usp.br>
2022-09-10 14:44:20 -0300

    test: enhance create method testing

--- test/entities/create.spec.js
+++ test/entities/create.spec.js
@@ -1,13 +1,28 @@
-import { bling, generators } from '../config/bling'
+import { bling, defaultBeforeEach, generators } from '../config/bling'
 
 jest.setTimeout(60000)
 
 const table = Object.keys(generators)
+const runs = 3
 
-test.each(table)(
-  'should create an entity of type "%s" when calling `.create()` method',
-  async (name) => {
-    const entity = generators[name]()
-    await expect(bling[name]().create(entity)).resolves.toBeDefined()
-  }
-)
+beforeEach(() => {
+  return defaultBeforeEach()
+})
+
+for (let i = 0; i < runs; i++) {
+  test.each(table)(
+    'should create an entity of type "%s" when calling `.create()` method',
+    async (name) => {
+      const entity = generators[name]()
+
+      try {
+        const promise = await bling[name]().create(entity)
+
+        expect(promise).toBeDefined()
+        expect(promise).toBeTruthy()
+      } catch (err) {
+        fail(`name: ${name}, ${JSON.stringify(entity)}, ${err.data.errors}`)
+      }
+    }
+  )
+}
