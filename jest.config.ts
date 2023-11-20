fc075f3a64c4802c4a06661d70735a9b4cdf826d
Alexandre Batistella Bellas <alexandre.bellas@gmail.com>
2023-11-20 16:08:14 -0300

    chore: melhorar arquivos do projeto para teste

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'jest'
+import { pathsToModuleNameMapper } from 'ts-jest'
+import { compilerOptions } from './tsconfig.json'
+
+const config: Config = {
+  transform: {
+    '^.+\\.(t|j)sx?$': 'ts-jest'
+  },
+  testRegex: '(/test/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: ['/node_modules/', '\\.pnp\\.[^\\/]+$'],
+  testPathIgnorePatterns: ['<rootDir>/lib/'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/'
+  }),
+  maxWorkers: 1,
+  maxConcurrency: 2,
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: './coverage'
+}
+
+export default config
